e13fee97919acba5491b759899079aa647187096
MrDHat <ktyaks@gmail.com>
2012-06-23 18:02:39 +0530

    Added raindrops and collision events

--- drop/src/com/inox/drop/Drop.java
+++ drop/src/com/inox/drop/Drop.java
@@ -8,12 +8,12 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class Drop implements ApplicationListener {
 	
@@ -24,17 +24,29 @@ public class Drop implements ApplicationListener {
 	OrthographicCamera camera ;
 	SpriteBatch batch ;
 	Rectangle bucket ;
+	Array <Rectangle> raindrops ;
+	long lastDropTime ; //To record the last time a drop was spawned (in nanoseconds)
+	
+	public void spawnRaindrop(){ 	// Function to spawn rain drop at a random position 
+		Rectangle raindrop = new Rectangle();
+		raindrop.x = MathUtils.random(0,800-48);
+		raindrop.y = 480 ;
+		raindrop.width = 48 ;
+		raindrop.height = 48 ;
+		raindrops.add(raindrop);
+		lastDropTime = TimeUtils.nanoTime();
+	}
 	
 	@Override
 	public void create() {		
 		
 		//load Images 
-		dropImage = new Texture(Gdx.files.internal("droplet.png"));
-		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+		dropImage = new Texture(Gdx.files.internal("data/droplet.png"));
+		bucketImage = new Texture(Gdx.files.internal("data/bucket.png"));
 		
 		//loading sound effects
-		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav")) ;
-		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3")) ;
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("data/drop.wav")) ;
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("data/rain.mp3")) ;
 		
 		//Starting the playback of rainMusic
 		rainMusic.setLooping(true);
@@ -51,11 +63,19 @@ public class Drop implements ApplicationListener {
 		bucket.y = 20 ;
 		bucket.width = 48 ;
 		bucket.height = 48;
+		
+		//Load raindrops
+		raindrops = new Array<Rectangle>() ;
+		spawnRaindrop();
 	}
 
 	@Override
 	public void dispose() {
-		
+		dropImage.dispose();
+		bucketImage.dispose();
+		dropSound.dispose();
+	    rainMusic.dispose();
+	    batch.dispose();
 	}
 
 	@Override
@@ -64,10 +84,12 @@ public class Drop implements ApplicationListener {
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		camera.update() ;
 		
-		//rendering the bucket
+		//rendering the bucket and raindrop
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(bucketImage, bucket.x, bucket.y);
+		for(Rectangle raindrop: raindrops)
+			batch.draw(dropImage, raindrop.x, raindrop.y);
 		batch.end();
 		
 		//Bucket movement
@@ -90,8 +112,26 @@ public class Drop implements ApplicationListener {
 			bucket.x = 0;
 		if(bucket.y > 800 - 48)
 			bucket.x = 800-48 ;
+	
+		
+		if(TimeUtils.nanoTime()-lastDropTime > 1000000000)
+			spawnRaindrop() ;
+		
+		java.util.Iterator<Rectangle> iter = raindrops.iterator() ;  //Movement of raindrops
+		while(iter.hasNext()){
+			Rectangle raindrop = iter.next() ;
+			raindrop.y -= 200*Gdx.graphics.getDeltaTime();
+			if(raindrop.y + 48 < 0)
+				iter.remove();
+			if(raindrop.overlaps(bucket)){
+				dropSound.play();
+				iter.remove();
+			}
+		}
+		
+		
 	}
-
+	
 	@Override
 	public void resize(int width, int height) {
 	}
